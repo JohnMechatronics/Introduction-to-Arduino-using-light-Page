335581bba37078c5cc317b0e688037a302669d65
JohnMechatronics <32952197+JohnMechatronics@users.noreply.github.com>
2017-10-20 16:19:23 +0100

    Update 8LED_arrays_Loops.ino

--- 8LED_arrays_Loops.ino
+++ 8LED_arrays_Loops.ino
@@ -6,7 +6,7 @@ int ledPins[] = {6,7,8,9,10,11,12,13};
 
 void setup()
 {
-  for (int i=0;i<7;i++){
+  for (int i=0;i<8;i++){
     pinMode(ledPins[i],OUTPUT);
   }
 }
@@ -14,12 +14,12 @@ void setup()
 void loop()                     
 {
   //Loops turning each LED on one after another
-  for(int i = 0; i <= 7; i++){
+  for(int i = 0; i <= 8; i++){
     digitalWrite(ledPins[i], HIGH);  
     delay(100);                 
   }                                  
   //Loops turning each LED off one after another
-  for(int i = 7; i >= 0; i--){                             
+  for(int i = 8; i >= 0; i--){                             
     digitalWrite(ledPins[i], LOW);  
     delay(100);                
   }                                  
